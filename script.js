bf879e698fb8305c2723af4b2051c0a0fe8df2b2
Parimal Bakrola <pbakrola@markettime.com>
2022-12-14 15:10:52 +0530

    New logo added , Links working

---
+++ script.js
@@ -0,0 +1,9 @@
+// Logo Rotation
+// window.onscroll = function () {
+//     scrollRotate();
+// };
+
+// function scrollRotate() {
+//     let image = document.getElementById("ring");
+//     image.style.transform = "rotate(" + window.pageYOffset/50 + "deg)";
+// }
