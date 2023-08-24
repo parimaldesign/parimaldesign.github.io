3755054900af627ff743d5a6b0624bcb1b12cbca
Parimal Bakrola <pbakrola@markettime.com>
2023-08-24 11:07:34 +0530

    Loader added, Buttons changed

--- script.js
+++ script.js
@@ -7,3 +7,13 @@
 //     let image = document.getElementById("ring");
 //     image.style.transform = "rotate(" + window.pageYOffset/50 + "deg)";
 // }
+
+document.onreadystatechange = function () {
+    var state = document.readyState;
+    if (state == 'complete') {
+      setTimeout(function(){
+        document.getElementById('interactive');
+        document.getElementById('loader').style.display="none";
+      },700);
+    }
+}
